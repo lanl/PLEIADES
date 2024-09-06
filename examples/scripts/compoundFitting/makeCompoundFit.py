fb3d2b2c564eaaeb9a5acd85b19ecca5d7a6b0bf
Alexander Long <alexanderlong5@gmail.com>
2024-09-05 19:14:01 -0600

    reorging examples in main branch to match 13-calling-sammy branch

--- examples/scripts/compoundFitting/makeCompoundFit.py
+++
@@ -1,7 +0,0 @@
-from pleiades import sammyUtils
-
-# Load the configuration file from the ini file in the parent directory
-uranium = sammyUtils.SammyFitConfig('../configFiles/uranium.ini')
-
-# Create the needed parFiles from ENDF for the isotopes in the configuration file
-sammyUtils.create_parFile_from_endf(uranium,verbose_level=2)
