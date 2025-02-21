e4fa46c6ed22f6910c7bf191446b26c46c1fea7c
Alexander Long <alexlong@lanl.gov>
2025-02-21 12:39:40 -0700

    adding logger for debugging issues

---
+++ src/pleiades/utils/logger.py
@@ -0,0 +1,25 @@
+import logging
+
+class Logger:
+    def __init__(self, name: str, level: int = logging.INFO):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def debug(self, message: str):
+        self.logger.debug(message)
+
+    def info(self, message: str):
+        self.logger.info(message)
+
+    def warning(self, message: str):
+        self.logger.warning(message)
+
+    def error(self, message: str):
+        self.logger.error(message)
+
+    def critical(self, message: str):
+        self.logger.critical(message)
