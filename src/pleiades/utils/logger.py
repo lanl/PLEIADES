d80ef6b0d8d4995eb9cccaeddd83f3791efe598f
Alexander Long <alexlong@lanl.gov>
2025-02-21 14:07:56 -0700

    moved _log_and_raise_error() to logger.py

--- src/pleiades/utils/logger.py
+++ src/pleiades/utils/logger.py
@@ -1,13 +1,25 @@
 import logging
 
+def _log_and_raise_error(logger, message: str, exception_class: Exception):
+    logger.error(message)
+    raise exception_class(message)
+
 class Logger:
-    def __init__(self, name: str, level: int = logging.INFO):
+    def __init__(self, name: str, level: int = logging.DEBUG, log_file: str = None):
+        
+        # 
         self.logger = logging.getLogger(name)
         self.logger.setLevel(level)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        
+        # Create file handler if log_file is specified
+        if log_file:
+            file_handler = logging.FileHandler(log_file)
+            file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            file_handler.setFormatter(file_formatter)
+            self.logger.addHandler(file_handler)
+
+        # Log a break line
+        self.logger.info("logging initialized...")
 
     def debug(self, message: str):
         self.logger.debug(message)
@@ -23,3 +35,5 @@ class Logger:
 
     def critical(self, message: str):
         self.logger.critical(message)
+
+    
