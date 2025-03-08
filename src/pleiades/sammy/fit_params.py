b808325ade7cacd6c2f7f81e31fd9901219204f7
Alexander Long <alexlong@lanl.gov>
2025-03-08 14:14:28 -0700

    trying to figure out how to implement general fit options into the fit parameters file.

--- src/pleiades/sammy/fit_params.py
+++ src/pleiades/sammy/fit_params.py
@@ -3,10 +3,7 @@ from pydantic import BaseModel, Field, constr
 from pleiades.core.nuclear_params import nuclearParameters
 from pleiades.core.physics_params import PhysicsParameters
 
-from pleiades.sammy import alphanumerics
-
-#Fitting comman
-
+import pleiades.sammy.fit_options as fit_options 
 
 
 class DataType(str, Enum):
@@ -40,17 +37,6 @@ class FitRoutineParameters(BaseModel):
     i_correlation: int = Field(description="Correlation matrix interval", Default=50)
     endf_mat_num: int = Field(description="ENDF material number", Default=0)
 
-class AlphanumericParameters(BaseModel):
-    """Container for alphanumeric commands that could be used in fitting.
-
-    Attributes:
-        r_matrix_options: R-matrix approximation options
-        input_covariance_matrix_options: Prior covariance matrix input options
-    """    
-    r_matrix_options: alphanumerics.rm.RMatrixOptions = Field(description="R-matrix approximation options")
-    input_covariance_matrix_options: alphanumerics.pcm_in.CovarianceMatrixOptions = Field(description="Prior covariance matrix input options")
-    quantum_parameter_options: alphanumerics.params.ParameterOptions = Field(description="Parameter options")
-
 
 class dataParameters(BaseModel):
     """Container for data parameters.
@@ -79,12 +65,18 @@ class FitParameters(BaseModel):
         physics_params (PhysicsParameters): Physics parameters including energy, normalization, and broadening.
     """
 
-    fit_title: str = Field(description="Title of fitting run")
+    fit_title: str = Field(description="Title of fitting run", default="SAMMY Fit")
     tolerance: float = Field(description="Tolerance for chi-squared convergence")
     max_cpu_time: float = Field(description="Maximum CPU time allowed")
     max_wall_time: float = Field(description="Maximum wall time allowed")
     max_memory: float = Field(description="Maximum memory allowed")
     max_disk: float = Field(description="Maximum disk space allowed")
 
+    data_params: dataParameters = Field(description="Data parameters used in SAMMY calculations")
+    fit_routine_params: FitRoutineParameters = Field(description="Fit routine parameters used in SAMMY calculations")
+    
     nuclear_params: nuclearParameters = Field(description="Nuclear parameters used in SAMMY calculations")
     physics_params: PhysicsParameters = Field(description="Physics parameters used in SAMMY calculations")
+
+    # fitting parameters and routines to be used. 
+    options_and_routines: fit_options.FitOptions = Field(description="Fit options used in SAMMY calculations")
