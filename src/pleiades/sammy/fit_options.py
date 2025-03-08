759cd1f4c46ce1533d0571ae0d6d83477336aa4d
Alexander Long <alexlong@lanl.gov>
2025-03-08 14:13:18 -0700

    adding general fit option class.

---
+++ src/pleiades/sammy/fit_options.py
@@ -0,0 +1,48 @@
+from enum import Enum
+from pydantic import BaseModel, Field, Optional
+
+class RMatrixOptions(str, Enum):
+    REICH_MOORE_FORMALISM = "REICH-MOORE FORMALISM"
+    ORIGINAL_REICH_MOORE = "ORIGINAL REICH-MOORE"
+    MULTILEVEL_BREITWIGNER = "MULTILEVEL BREITWIGNER"
+    SINGLE_LEVEL_BREITWIGNER = "SINGLE LEVEL BREITWIGNER"
+    REDUCED_WIDTH_AMPLITUDES = "REDUCED WIDTH AMPLITUDES"
+
+class SpinGroupOptions(str, Enum):
+    """Parameter input options."""
+    USE_NEW_SPIN_GROUP = "USE NEW SPIN GROUP"
+    PARTICLE_PAIR_DEFINITION = "PARTICLE PAIR DEFINITION"
+    KEY_WORD_PARTICLE_PAIR = "KEY WORD PARTICLE PAIR"
+
+class QuantumNumbersOptions(str, Enum):
+    """Quantum numbers options."""
+    Q_NUMBERS_IN_PARAM_FILE = "QUANTUM NUMBERS ARE IN PARAMETER FILE"
+    PUT_Q_NUMBERS_IN_PARAM_FILE = "PUT QUANTUM NUMBERS INTO PARAMETER FILE"
+
+class DataFormatOptions(str,Enum):
+    """ Data format options """
+    DATA_IN_ORIGINAL_MULTI_FORMAT = "DATA IN ORIGINAL MULTI-FORMAT"
+    DATA_IN_CSISRS_FORMAT = "DATA IN CSISRS FORMAT"
+    DATA_IN_TWENTY_FORMAT = "DATA IN TWENTY FORMAT"
+    DATA_IN_STANDARD_FORMAT = "DATA IN STANDARD FORMAT"
+    DATA_IN_ODF_FORMAT = "DATA IN ODF FORMAT"
+    DATA_IN_ENDF_FORMAT = "DATA IN ENDF FORMAT"
+
+class BroadeningTypeOptions(str, Enum):
+    FREE_GAS_MODEL = "FREE GAS MODEL"
+    LEAL_HWANG_DOPPLER_MODEL = "LEAL-HWANG DOPPLER MODEL"
+    HIGH_ENERGY_GAUSSIAN = "HIGH ENERGY GAUSSIAN"
+    CRYSTAL_LATTICE_MODEL = "CRYSTAL LATTICE MODEL"
+
+
+# a class to hold all the fit options
+class FitOptions(BaseModel):
+    """Container for fit options with SAMMY"""
+    RMatrix: RMatrixOptions = Field(description="R-matrix option for the calculation", default=RMatrixOptions.REICH_MOORE_FORMALISM)
+    SpinGroupFormat: SpinGroupOptions = Field(description="Parameter input options", default=SpinGroupOptions.USE_NEW_SPIN_GROUP)
+    QuantumNumbers: QuantumNumbersOptions = Field(description="Quantum numbers options", default=QuantumNumbersOptions.Q_NUMBERS_IN_PARAM_FILE)
+    input_is_endf_b_file_2: bool = Field(description="Indicates if the input is ENDF/B FILE 2", default=False)
+    DataFormat: DataFormatOptions = Field(description="Data format options", default=DataFormatOptions.DATA_IN_ORIGINAL_MULTI_FORMAT)
+    ImplementBroadeningOption: bool = Field(description="option to implement Broadening", default=False)
+    BroadeningType: Optional[BroadeningTypeOptions] = Field(description="Broadening type options", default=None)
+    SolveBayesEquation: bool = Field(description="option to solve Bayes equation", default=False)
