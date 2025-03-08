c083c7959975efe5cff1b7dc73c4743e9a48b281
Alexander Long <alexlong@lanl.gov>
2025-03-08 16:59:01 -0700

    making fit options tests

--- src/pleiades/sammy/fit_options.py
+++ src/pleiades/sammy/fit_options.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from pydantic import BaseModel, Field, Optional
+from pydantic import BaseModel, Field, root_validator
 
 class RMatrixOptions(str, Enum):
     REICH_MOORE_FORMALISM = "REICH-MOORE FORMALISM"
@@ -19,6 +19,7 @@ class QuantumNumbersOptions(str, Enum):
     Q_NUMBERS_IN_PARAM_FILE = "QUANTUM NUMBERS ARE IN PARAMETER FILE"
     PUT_Q_NUMBERS_IN_PARAM_FILE = "PUT QUANTUM NUMBERS INTO PARAMETER FILE"
 
+
 class DataFormatOptions(str,Enum):
     """ Data format options """
     DATA_IN_ORIGINAL_MULTI_FORMAT = "DATA IN ORIGINAL MULTI-FORMAT"
@@ -34,6 +35,10 @@ class BroadeningTypeOptions(str, Enum):
     HIGH_ENERGY_GAUSSIAN = "HIGH ENERGY GAUSSIAN"
     CRYSTAL_LATTICE_MODEL = "CRYSTAL LATTICE MODEL"
 
+class PrintInputOptions(str, Enum):
+    DO_NOT_PRINT_ANY_INPUT = "DO NOT PRINT ANY INPUT"
+    PRINT_ALL_INPUT = "PRINT ALL INPUT"
+    PRINT_VARIED_INPUT = "PRINT VARIED INPUT"
 
 # a class to hold all the fit options
 class FitOptions(BaseModel):
@@ -44,5 +49,17 @@ class FitOptions(BaseModel):
     input_is_endf_b_file_2: bool = Field(description="Indicates if the input is ENDF/B FILE 2", default=False)
     DataFormat: DataFormatOptions = Field(description="Data format options", default=DataFormatOptions.DATA_IN_ORIGINAL_MULTI_FORMAT)
     ImplementBroadeningOption: bool = Field(description="option to implement Broadening", default=False)
-    BroadeningType: Optional[BroadeningTypeOptions] = Field(description="Broadening type options", default=None)
+    BroadeningType: BroadeningTypeOptions = Field(description="Broadening type options", default=None)
     SolveBayesEquation: bool = Field(description="option to solve Bayes equation", default=False)
+    PrintInputDataInLPT: bool = Field(description="option to print input data in LPT", default=False)
+    PrintInputParamsInLPT: PrintInputOptions = Field(description="option to print input parameters in LPT", default=PrintInputOptions.DO_NOT_PRINT_ANY_INPUT)
+
+    # set the default broadening type to FGM if the broadening option is set
+    @root_validator(pre=True)
+    def set_broadening_type(cls, values):
+        if values.get('ImplementBroadeningOption'):
+            if 'BroadeningType' not in values or values['BroadeningType'] is None:
+                values['BroadeningType'] = BroadeningTypeOptions.FREE_GAS_MODEL
+        else:
+            values['BroadeningType'] = None
+        return values
