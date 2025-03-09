e06b0b0512597a5f6738a6cbe1d846a9c02aa17c
Alexander Long <alexanderlong5@gmail.com>
2025-03-08 19:57:24 -0700

    refactor unit options and add unit tests for energy and cross-section conversions

--- src/pleiades/utils/units.py
+++ src/pleiades/utils/units.py
@@ -9,15 +9,13 @@ class EnergyUnitOptions(str, Enum):
     MeV = "MeV"
     GeV = "GeV"
     J = "J"
-    erg = "erg"
 
 class CrossSectionUnitOptions(str, Enum):
     barn = "barn"
     millibarn = "millibarn"
     microbarn = "microbarn"
     cm2 = "cm^2"
-    m2 = "m^2"
-    
+
 def convert_to_energy(from_unit, to_unit):
     """Convert energy from one unit to another unit
     based on EnergyUnitOptions options
@@ -37,8 +35,7 @@ def convert_to_energy(from_unit, to_unit):
         EnergyUnitOptions.keV: 1e3,
         EnergyUnitOptions.MeV: 1e6,
         EnergyUnitOptions.GeV: 1e9,
-        EnergyUnitOptions.J: 6.242e12,
-        EnergyUnitOptions.erg: 6.242e11
+        EnergyUnitOptions.J: 6.242e12
     }
     
     return conversion_factors[from_unit] / conversion_factors[to_unit]
@@ -60,8 +57,7 @@ def convert_to_cross_section(from_unit, to_unit):
         CrossSectionUnitOptions.barn: 1,
         CrossSectionUnitOptions.millibarn: 1e-3,
         CrossSectionUnitOptions.microbarn: 1e-6,
-        CrossSectionUnitOptions.cm2: 1e-24,
-        CrossSectionUnitOptions.m2: 1e-4
+        CrossSectionUnitOptions.cm2: 1e-24
     }
     
     return conversion_factors[from_unit] / conversion_factors[to_unit]
