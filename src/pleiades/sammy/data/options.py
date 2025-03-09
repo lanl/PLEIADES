ce1c8d6325555e3a6a015752ce493a26f73020d3
Alexander Long <alexanderlong5@gmail.com>
2025-03-08 19:44:04 -0700

    got rid of converting unit functions.

--- src/pleiades/sammy/data/options.py
+++ src/pleiades/sammy/data/options.py
@@ -1,9 +1,7 @@
 from enum import Enum
-from pydantic import BaseModel, Field
-import pint
+from pydantic import BaseModel, Field, ConfigDict
 
-# Create a unit registry
-ureg = pint.UnitRegistry()
+from pleiades.utils.units import EnergyUnitOptions, CrossSectionUnitOptions
 
 class DataTypeOptions(str, Enum):
     TRANSMISSION = "TRANSMISSION"
@@ -20,6 +18,8 @@ class DataTypeOptions(str, Enum):
     INTEGRAL = "INTEGRAL"
     COMBINATION = "COMBINATION"
 
+
+
 class dataParameters(BaseModel):
     """Container for data parameters.
 
@@ -35,27 +35,7 @@ class dataParameters(BaseModel):
 
     data_file: str = Field(description="File containing the data", default=None)
     data_type: DataTypeOptions = Field(description="Type of the data", default=DataTypeOptions.TRANSMISSION)
-    energy_units: pint.Quantity = Field(description="Units of energy", default=ureg.eV)
-    cross_section_units: pint.Quantity = Field(description="Units of cross-section", default=ureg.barn)
+    energy_units: EnergyUnitOptions= Field(description="Units of energy", default=EnergyUnitOptions.eV)
+    cross_section_units: CrossSectionUnitOptions = Field(description="Units of cross-section", default=CrossSectionUnitOptions.barn)
     data_title: str = Field(description="Title of the data", default=None)
     data_comment: str = Field(description="Comment for the data", default=None)
-
-    def convert_energy_units(self, value, from_units, to_units):
-        """Convert energy value from specified units to specified units."""
-        energy = value * ureg(from_units)
-        converted_energy = energy.to(to_units)
-        return converted_energy.magnitude
-        
-    def convert_cross_section_units(self, value, from_units, to_units):
-        """Convert cross-section value from specified units to specified units."""
-        cross_section = value * ureg(from_units)
-        converted_cross_section = cross_section.to(to_units)
-        return converted_cross_section.magnitude
-
-# Example usage
-if __name__ == "__main__":
-    params = dataParameters()
-    energy_in_keV = params.convert_energy_units(1000, 'eV', 'keV')
-    cross_section_in_cm2 = params.convert_cross_section_units(1, 'barn', 'cm^2')
-    print(f"1000 eV is equal to {energy_in_keV} keV")
-    print(f"1 barn is equal to {cross_section_in_cm2} cm^2")
