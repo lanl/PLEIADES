0541155a54983bff2b4e34d12d3cea59e9289291
Alexander Long <alexanderlong5@gmail.com>
2025-03-08 17:40:55 -0700

    moved dataType option to option.py in data dir

--- src/pleiades/sammy/fit_params.py
+++ src/pleiades/sammy/fit_params.py
@@ -2,24 +2,24 @@ from enum import Enum
 from pydantic import BaseModel, Field, constr
 from pleiades.core.nuclear_params import nuclearParameters
 from pleiades.core.physics_params import PhysicsParameters
+from pleiades.sammy.data.options import dataParameters
 
 import pleiades.sammy.fit_options as fit_options 
 
-
-class DataType(str, Enum):
-    TRANSMISSION = "TRANSmission"
-    TOTAL_CROSS_SECTION = "TOTAL cross section"
-    SCATTERING = "SCATTering"
-    ELASTIC = "ELASTic"
-    DIFFERENTIAL_ELASTIC = "DIFFErential elastic"
-    DIFFERENTIAL_REACTION = "DIFFErentiAL REaction"
-    REACTION = "REACTion"
-    INELASTIC_SCATTERING = "INELAstic scattering"
-    FISSION = "FISSion"
-    CAPTURE = "CAPTure"
-    SELF_INDICATION = "SELF-indication"
-    INTEGRAL = "INTEGral"
-    COMBINATION = "COMBInation"
+class DataTypeOptions(str, Enum):
+    TRANSMISSION = "TRANSMISSION"
+    TOTAL_CROSS_SECTION = "TOTAL CROSS SECTION"
+    SCATTERING = "SCATTERING"
+    ELASTIC = "ELASTIC"
+    DIFFERENTIAL_ELASTIC = "DIFFERENTIAL ELASTIC"
+    DIFFERENTIAL_REACTION = "DIFFERENTIAL REACTION"
+    REACTION = "REACTION"
+    INELASTIC_SCATTERING = "INELASTIC SCATTERING"
+    FISSION = "FISSION"
+    CAPTURE = "CAPTURE"
+    SELF_INDICATION = "SELF INDICATION"
+    INTEGRAL = "INTEGRAL"
+    COMBINATION = "COMBINATION"
 
 class FitRoutineParameters(BaseModel):
     """Container for fit routine parameters.
@@ -33,29 +33,10 @@ class FitRoutineParameters(BaseModel):
         max_disk: Maximum disk space allowed
     """
 
-    max_iterations: int = Field(description="Maximum number of iterations")
-    i_correlation: int = Field(description="Correlation matrix interval", Default=50)
-    endf_mat_num: int = Field(description="ENDF material number", Default=0)
+    max_iterations: int = Field(description="Maximum number of iterations", default=1)
+    i_correlation: int = Field(description="Correlation matrix interval", default=50)
+    endf_mat_num: int = Field(description="ENDF material number", default=0)
 
-
-class dataParameters(BaseModel):
-    """Container for data parameters.
-
-    Attributes:
-        data_file: File containing the data
-        data_format: Format of the data
-        data_type: Type of the data
-        data_units: Units of the data
-        data_title: Title of the data
-        data_comment: Comment for the data
-    """
-
-    data_file: str = Field(description="File containing the data")
-    data_format: str = Field(description="Format of the data")
-    data_type: DataType = Field(description="Type of the data")
-    data_units: str = Field(description="Units of the data")
-    data_title: str = Field(description="Title of the data")
-    data_comment: str = Field(description="Comment for the data")
 
 class FitParameters(BaseModel):
     """Container for fit parameters including nuclear and physics parameters.
