248b65db362b6b2dc58db94a5fd80b3e155bc5ba
Alexander Long <alexlong@lanl.gov>
2025-04-11 12:47:03 -0600

    fix: remove skip decorator from TestDetectorEfficiencyParameters as functionality is now implemented

--- tests/unit/pleiades/sammy/parameters/test_det_efficiency.py
+++ tests/unit/pleiades/sammy/parameters/test_det_efficiency.py
@@ -9,7 +9,6 @@ import pytest
 from pleiades.sammy.parameters.det_efficiency import DetectorEfficiencyParameters
 
 
-@pytest.mark.skip(reason="Tests are disabled due to non-implementation.")
 class TestDetectorEfficiencyParameters:
     """Test suite for Card Set 15 (Detector Efficiency) parameter parsing and formatting.
 
