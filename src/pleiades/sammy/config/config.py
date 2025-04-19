53c12791652b2a2088f9a2db91efa01a89589a25
Alexander Long <alexlong@lanl.gov>
2025-04-18 21:32:16 -0600

    moved configuration manager for SAMMY execution backends with validation to sammy/config dir

--- src/pleiades/sammy/config/config.py
+++ src/pleiades/sammy/config/config.py
@@ -11,7 +11,7 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Dict
 
-from pleiades.sammy.interface import BaseSammyConfig, ConfigurationError
+from pleiades.sammy.config.interface import BaseSammyConfig, ConfigurationError
 
 
 @dataclass
