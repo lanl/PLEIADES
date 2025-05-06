338afc37cf46a445746810ef3ebdbddcdab52b71
Alexander M. Long <alexlong@lanl.gov>
2025-05-06 16:00:07 -0600

    Refactor ResultsManager to enhance LptManager initialization with RunResults and remove unused parameters.

--- src/pleiades/sammy/results/manager.py
+++ src/pleiades/sammy/results/manager.py
@@ -1,3 +1,5 @@
+import json
+
 from pleiades.sammy.io.lpt_manager import LptManager
 from pleiades.sammy.results.models import FitResults, RunResults
 from pleiades.utils.logger import loguru_logger
@@ -13,32 +15,24 @@ class ResultsManager:
         run_results (RunResults): A container for multiple fit results.
     """
 
+    # initialize the ResultsManager with a fit result class
+    def __init__(self):
+        self.run_results = RunResults()
+
     # Initialize a LptManager object to manage the LPT file
     def __init__(
         self,
         lpt_file_path: str = None,
         lst_file_path: str = None,
-        par_file_path: str = None,
-        inp_file_path: str = None,
     ):
-        self.lpt_manager = None
-        self.lst_manager = None
-        self.par_manager = None
-        self.inp_manager = None
         self.run_results = RunResults()
 
         # Initialize the managers based on the provided file paths
         # If a file path is provided, process the file and extract results.
         if lpt_file_path is not None:
-            self.lpt_manager = LptManager(lpt_file_path)
+            self.lpt_manager = LptManager(lpt_file_path, self.run_results)
         if lst_file_path is not None:
             # self.lst_manager = LstManager(lst_file_path)  # Implement if needed
-            pass
-        if par_file_path is not None:
-            # self.par_manager = ParManager(par_file_path)  # Implement if needed
-            pass
-        if inp_file_path is not None:
-            # self.inp_manager = InpManager(inp_file_path)  # Implement if needed
             pass
 
     def add_fit_result(self, fit_result: FitResults):
@@ -51,3 +45,22 @@ class ResultsManager:
             return self.run_results.fit_results[index]
         else:
             raise ValueError("No fit results available.")
+
+    def print_fit_result(self, index: int):
+        """Print a specific fit result in a readable format."""
+        fit_result = self.get_single_fit_results(index)
+        if fit_result:
+            logger.info(
+                f"Fit Result {index}:\n{json.dumps(fit_result.model_dump(), indent=2, default=str)}"
+            )
+        else:
+            logger.warning(f"No fit result found at index {index}.")
+
+    # Print the run results in a readable format
+    def print_run_results(self):
+        """Print the run results in a readable format."""
+        if self.run_results.fit_results:
+            for fit_result in self.run_results.fit_results:
+                logger.info(f"Fit Result: {fit_result}")
+        else:
+            logger.warning("No fit results available.")
