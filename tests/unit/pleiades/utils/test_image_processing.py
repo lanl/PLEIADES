2b54e6ecd3d6f18fe518bc0a3fa1b0b8b0d30897
JeanBilheux <bilheuxjm@ornl.gov>
2025-05-06 10:58:58 -0400

    all unit tests for rebin. this refs #57

--- tests/unit/pleiades/utils/test_image_processing.py
+++ tests/unit/pleiades/utils/test_image_processing.py
@@ -18,42 +18,40 @@ def test_rebin_with_valid_input():
                                  [[3.5, 5.5],
                                  [11.5, 13.5]]])
     result = rebin(data, binning_factor)
-    print(f"{result=}")
-    print(f"{expected_output=}")
     np.testing.assert_array_almost_equal(result, expected_output)
 
 
-# def test_rebin_with_non_divisible_dimensions():
-#     # Test with a 2D array where dimensions are not divisible by binning factor
-#     data = np.array([[1, 2, 3],
-#                      [4, 5, 6],
-#                      [7, 8, 9]])
-#     binning_factor = 2
-#     with pytest.raises(ValueError):
-#         rebin(data, binning_factor)
+def test_rebin_with_non_divisible_dimensions():
+    # Test with a 2D array where dimensions are not divisible by binning factor
+    data = np.array([[1, 2, 3],
+                     [4, 5, 6],
+                     [7, 8, 9]])
+    binning_factor = 2
+    with pytest.raises(ValueError):
+        rebin(data, binning_factor)
 
 
-# def test_rebin_with_invalid_binning_factor():
-#     # Test with an invalid binning factor (e.g., 0 or negative)
-#     data = np.array([[1, 2],
-#                      [3, 4]])
-#     binning_factor = 0
-#     with pytest.raises(ValueError):
-#         rebin(data, binning_factor)
+def test_rebin_with_invalid_binning_factor():
+    # Test with an invalid binning factor (e.g., 0 or negative)
+    data = np.array([[1, 2],
+                     [3, 4]])
+    binning_factor = 0
+    with pytest.raises(ValueError):
+        rebin(data, binning_factor)
 
 
-# def test_rebin_with_empty_array():
-#     # Test with an empty array
-#     data = np.array([[]])
-#     binning_factor = 2
-#     with pytest.raises(ValueError):
-#         rebin(data, binning_factor)
+def test_rebin_with_empty_array():
+    # Test with an empty array
+    data = np.array([[]])
+    binning_factor = 2
+    with pytest.raises(ValueError):
+        rebin(data, binning_factor)
 
 
-# def test_rebin_with_large_binning_factor():
-#     # Test with a binning factor larger than the array dimensions
-#     data = np.array([[1, 2],
-#                      [3, 4]])
-#     binning_factor = 3
-#     with pytest.raises(ValueError):
-#         rebin(data, binning_factor)
+def test_rebin_with_large_binning_factor():
+    # Test with a binning factor larger than the array dimensions
+    data = np.array([[1, 2],
+                     [3, 4]])
+    binning_factor = 3
+    with pytest.raises(ValueError):
+        rebin(data, binning_factor)
