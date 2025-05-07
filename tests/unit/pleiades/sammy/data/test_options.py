6e52e5bc747203c70eeb33f0e1ece7232131e1a0
Alexander M. Long <alexlong@lanl.gov>
2025-05-07 16:38:23 -0600

    Refactor tests to replace dataParameters with sammyData and update assertions for default and custom values

--- tests/unit/pleiades/sammy/data/test_options.py
+++ tests/unit/pleiades/sammy/data/test_options.py
@@ -1,44 +1,40 @@
 import pytest
+from pathlib import Path
 
-from pleiades.sammy.data.options import DataTypeOptions, dataParameters
+from pleiades.sammy.data.options import DataTypeOptions, sammyData
 from pleiades.utils.units import CrossSectionUnitOptions, EnergyUnitOptions
 
 
-def test_data_parameters_defaults():
-    """Test the default values of dataParameters."""
-    params = dataParameters()
+def test_sammy_data_defaults():
+    """Test the default values of sammyData."""
+    params = sammyData()
 
     assert params.data_file is None
     assert params.data_type == DataTypeOptions.TRANSMISSION
     assert params.energy_units == EnergyUnitOptions.eV
     assert params.cross_section_units == CrossSectionUnitOptions.barn
-    assert params.data_title is None
-    assert params.data_comment is None
+    assert params.data is None
 
 
-def test_data_parameters_custom_values():
-    """Test custom values of dataParameters."""
-    params = dataParameters(
-        data_file="custom.dat",
+def test_sammy_data_custom_values():
+    """Test custom values of sammyData."""
+    params = sammyData(
+        data_file=Path("custom.dat"),
         data_type=DataTypeOptions.CAPTURE,
         energy_units=EnergyUnitOptions.keV,
         cross_section_units=CrossSectionUnitOptions.millibarn,
-        data_title="Custom Data",
-        data_comment="This is a custom data set.",
     )
 
-    assert params.data_file == "custom.dat"
+    assert params.data_file == Path("custom.dat")
     assert params.data_type == DataTypeOptions.CAPTURE
     assert params.energy_units == EnergyUnitOptions.keV
     assert params.cross_section_units == CrossSectionUnitOptions.millibarn
-    assert params.data_title == "Custom Data"
-    assert params.data_comment == "This is a custom data set."
 
 
 def test_invalid_data_type():
     """Test invalid data type."""
     with pytest.raises(ValueError):
-        dataParameters(data_file="invalid.dat", data_type="INVALID_TYPE")
+        sammyData(data_file=Path("invalid.dat"), data_type="INVALID_TYPE")
 
 
 if __name__ == "__main__":
