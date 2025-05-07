1b5d38dc1d9ee9d970188d09fcc94238ea135019
Alexander M. Long <alexlong@lanl.gov>
2025-05-07 14:24:13 -0600

    Refactor LstManager to use sammyData instead of LstData and update type annotations for improved clarity

--- src/pleiades/sammy/io/lst_manager.py
+++ src/pleiades/sammy/io/lst_manager.py
@@ -1,4 +1,5 @@
-from pleiades.sammy.data.options import LstData
+from pathlib import Path
+from pleiades.sammy.data.options import sammyData
 from pleiades.sammy.results.models import RunResults
 
 
@@ -10,21 +11,21 @@ class LstManager:
     object to store the results.
 
     Attributes:
-        lst_file_path (str): Path to the SAMMY LST file.
-        data (LstData): An instance of LstData to hold the extracted data.
+        lst_file_path (Path): Path to the SAMMY LST file.
+        data (sammyData): An instance of sammyData to hold the extracted data.
     Methods:
-        __init__(lst_file_path: str):
+        __init__(lst_file_path: Path, run_results: RunResults = None):
             Initializes the LstManager with the path to a SAMMY LST file.
         process_lst_file():
-            Parses the SAMMY LST file and extracts relevant data in a LstData object
+            Parses the SAMMY LST file and extracts relevant data in a sammyData object
     """
 
-    def __init__(self, lst_file_path: str, run_results: RunResults = None):
+    def __init__(self, lst_file_path: Path = None, run_results: RunResults = None):
         """
         Initialize the LstManager with the path to a SAMMY LST file.
 
         Args:
-            lst_file_path (str): Path to the SAMMY LST file.
+            lst_file_path (Path): Path to the SAMMY LST file.
             run_results (RunResults, optional): A container for multiple fit results.
                 If not provided, a new RunResults object will be created.
         """
@@ -34,16 +35,25 @@ class LstManager:
         else:
             self.run_results = RunResults()
 
-        if lst_file_path:
-            self.process_lst_file(lst_file_path, self.run_results)
+        # If lst_file_path is not None, and file exists, process the file
+        if lst_file_path is not None and isinstance(lst_file_path, Path):
+            if not lst_file_path.exists():
+                raise FileNotFoundError(f"The file {lst_file_path} does not exist.")
+            else:
+                self.process_lst_file(lst_file_path, self.run_results)
 
-    def process_lst_file(self, lst_file_path: str, run_results: RunResults):
+            
+            
+            
+            
+
+    def process_lst_file(self, lst_file_path: Path, run_results: RunResults):
         """
-        Parse the SAMMY LST file and extract relevant data in a LstData object.
-        The LstData object is stored in self.run_results.data.
+        Parse the SAMMY LST file and extract relevant data in a sammyData object.
+        The sammyData object is stored in self.run_results.data.
         """
-        # Create LstData and load the file
-        lst_data = LstData(data_file=lst_file_path)
+        # Create sammyData and load the file
+        lst_data = sammyData(data_file=lst_file_path)
         lst_data.load()  # Explicitly load and validate
 
         # Store in RunResults
