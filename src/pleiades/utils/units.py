Fixed conversion to energy units. this refs #57

src/pleiades/utils/units.py
@@ -135,21 +135,21 @@ def convert_to_cross_section(from_unit, to_unit):
     return conversion_factors[from_unit] / conversion_factors[to_unit]
 
 
-def convert_from_wavelength_to_energy_ev(wavelength, 
-                                         unit_from=DistanceUnitOptions.angstrom): 
-    """Convert wavelength to energy based on the given units.
+# def convert_from_wavelength_to_energy_ev(wavelength, 
+#                                          unit_from=DistanceUnitOptions.angstrom): 
+#     """Convert wavelength to energy based on the given units.
 
-    Args:
-        wavelength (float): Wavelength value.
-        unit_from (WavelengthUnitOptions): Unit of the input wavelength.
+#     Args:
+#         wavelength (float): Wavelength value.
+#         unit_from (WavelengthUnitOptions): Unit of the input wavelength.
 
-    Returns:
-        float: Energy in the new unit.
-    """
-    wavelength_m = wavelength * convert_distance_units(unit_from, DistanceUnitOptions.m)
-    energy = h * c / wavelength_m
-    energy = energy * convert_to_energy(EnergyUnitOptions.J, EnergyUnitOptions.eV)
-    return energy
+#     Returns:
+#         float: Energy in the new unit.
+#     """
+#     wavelength_m = wavelength * convert_distance_units(unit_from, DistanceUnitOptions.m)
+#     energy = h * c / wavelength_m
+#     energy = energy * convert_to_energy(EnergyUnitOptions.J, EnergyUnitOptions.eV)
+#     return energy
 
 
 def convert_array_from_time_to_lambda(time_array: np.ndarray, 
@@ -205,20 +205,31 @@ def convert_array_from_time_to_energy(time_array: np.ndarray,
         detector_offset_unit (DistanceUnitOptions): Unit of the offset.
         energy_unit (EnergyUnitOptions): Unit of the output energy.
 
-    This is using the formula: E = h/(m_n * distance_source_detector_m) * (time_array_s + detector_offset_s)
-
+    #This is using the formula: E = h/(m_n * distance_source_detector_m) * (time_array_s + detector_offset_s)
+    this is using the formula: E_ev = 1/2 m_n v^2
+    with t_tof = L/v (L distance source to detector in m, v velocity of the neutron in m/s)
+    so E_ev = 1/2 m_n (L/t_tof)^2
+    
     Returns:
         np.ndarray: Array of energy values.
     """
-    lambda_in_angstroms = convert_array_from_time_to_lambda(time_array, 
-                                                            time_unit, 
-                                                            distance_source_detector,
-                                                            distance_source_detector_unit, 
-                                                            detector_offset, 
-                                                            detector_offset_unit, 
-                                                            DistanceUnitOptions.angstrom)
+    
+    time_units_factor = convert_time_units(time_unit, TimeUnitOptions.s)
+    time_array_s = time_array * time_units_factor
+    
+    detector_units_factor = convert_time_units(detector_offset_unit, TimeUnitOptions.s)
+    detector_offset = detector_units_factor * detector_offset
 
-    energy_array_ev = [convert_from_wavelength_to_energy_ev(l, DistanceUnitOptions.angstrom) for l in lambda_in_angstroms]
-    energy_array = np.array(energy_array_ev)
+    distance_source_detector_factor = convert_distance_units(distance_source_detector_unit, DistanceUnitOptions.m)
+    distance_source_detector_m = distance_source_detector * distance_source_detector_factor
+    
+    # Calculate the energy in eV using the formula E_ev = 1/2 m_n (L/t_tof)^2 / electron_volt
+
+    full_time_array_s = time_array_s + detector_offset
+    energy_array_ev = 0.5 * m_n * (distance_source_detector_m / full_time_array_s) ** 2 / electron_volt
+
+    energy_array_factor = convert_to_energy(EnergyUnitOptions.eV, energy_unit)
+    energy_array = energy_array_ev * energy_array_factor
+    energy_array = np.array(energy_array)
 
     return energy_array

tests/unit/pleiades/utils/test_units.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.constants import h, c, electron_volt, m_n
 
 from pleiades.utils.units import CrossSectionUnitOptions, EnergyUnitOptions, TimeUnitOptions, convert_to_cross_section, convert_to_energy, convert_time_units
-from pleiades.utils.units import convert_from_wavelength_to_energy_ev, DistanceUnitOptions, convert_array_from_time_to_lambda
+from pleiades.utils.units import DistanceUnitOptions, convert_array_from_time_to_lambda, convert_array_from_time_to_energy
 
 
 def test_convert_to_energy():
@@ -17,18 +17,6 @@ def test_convert_to_cross_section():
     assert convert_to_cross_section(CrossSectionUnitOptions.barn, CrossSectionUnitOptions.millibarn) == 1e3
     assert convert_to_cross_section(CrossSectionUnitOptions.microbarn, CrossSectionUnitOptions.barn) == 1e-6
 
-def test_convert_from_wavelength_to_energy():
-    # Test conversion from angstrom to eV
-    wavelength_angstrom = 3.5  # 3.5 angstrom
-    wavelength_m = 3.5e-10
-    expected_energy_eV = (h * c) / (wavelength_m * electron_volt)
-    assert convert_from_wavelength_to_energy_ev(wavelength_angstrom, 
-                                               unit_from=DistanceUnitOptions.angstrom) == pytest.approx(expected_energy_eV)
-
-    # Test conversion from nm to eV
-    wavelength_nm = 1.0  # 1 nm
-    expected_energy_eV = h * c / (wavelength_nm * 1e-9 * electron_volt)
-    assert convert_from_wavelength_to_energy_ev(wavelength_nm, DistanceUnitOptions.nm) == pytest.approx(expected_energy_eV)
 
 def test_convert_time_units():
     # Test conversion from seconds to milliseconds
@@ -101,6 +89,87 @@ def test_convert_array_from_time_to_lambda():
         time_array, time_unit, distance_source_detector, distance_source_detector_unit, detector_offset, detector_offset_unit, lambda_unit
     )
     assert np.allclose(result, expected_lambda)
+
+def test_convert_array_from_time_to_energy_basic():
+    # Simple test: 1, 2, 3 microseconds, 5 meters, no offset, output in eV
+    time_array = np.array([1.0, 2.0, 3.0])
+    time_unit = TimeUnitOptions.us
+    distance_source_detector = 5.0
+    distance_source_detector_unit = DistanceUnitOptions.m
+    detector_offset = 0.0
+    detector_offset_unit = TimeUnitOptions.us
+    energy_unit = EnergyUnitOptions.eV
+
+    # E_ev = 0.5 * m_n * (L / (t + offset))^2 / electron_volt
+    t_s = time_array * 1e-6
+    L_m = distance_source_detector
+    expected = 0.5 * m_n * (L_m / t_s) ** 2 / electron_volt
+
+    result = convert_array_from_time_to_energy(
+        time_array, time_unit, distance_source_detector, distance_source_detector_unit,
+        detector_offset, detector_offset_unit, energy_unit
+    )
+    assert np.allclose(result, expected)
+
+
+def test_convert_array_from_time_to_energy_with_offset_and_units():
+    # Test with offset and different units
+    time_array = np.array([1000.0, 2000.0, 3000.0])  # ns
+    time_unit = TimeUnitOptions.ns
+    distance_source_detector = 500.0  # cm
+    distance_source_detector_unit = DistanceUnitOptions.cm
+    detector_offset = 0.5  # us
+    detector_offset_unit = TimeUnitOptions.us
+    energy_unit = EnergyUnitOptions.eV
+
+    t_s = time_array * 1e-9 + detector_offset * 1e-6
+    L_m = distance_source_detector * 1e-2
+    expected = 0.5 * m_n * (L_m / t_s) ** 2 / electron_volt
+
+    result = convert_array_from_time_to_energy(
+        time_array, time_unit, distance_source_detector, distance_source_detector_unit,
+        detector_offset, detector_offset_unit, energy_unit
+    )
+    assert np.allclose(result, expected)
+
+
+def test_convert_array_from_time_to_energy_to_keV():
+    # Test output in keV
+    time_array = np.array([5000.0])  # ns
+    time_unit = TimeUnitOptions.ns
+    distance_source_detector = 2.0  # m
+    distance_source_detector_unit = DistanceUnitOptions.m
+    detector_offset = 0.0
+    detector_offset_unit = TimeUnitOptions.ns
+    energy_unit = EnergyUnitOptions.keV
+
+    t_s = time_array * 1e-9
+    L_m = distance_source_detector
+    expected_eV = 0.5 * m_n * (L_m / t_s) ** 2 / electron_volt
+    expected_keV = expected_eV * 1e-3
+
+    result = convert_array_from_time_to_energy(
+        time_array, time_unit, distance_source_detector, distance_source_detector_unit,
+        detector_offset, detector_offset_unit, energy_unit
+    )
+    assert np.allclose(result, expected_keV)
+
+
+def test_convert_array_from_time_to_energy_zero_time_raises():
+    # Should handle division by zero gracefully (return inf or nan)
+    time_array = np.array([0.0])
+    time_unit = TimeUnitOptions.s
+    distance_source_detector = 1.0
+    distance_source_detector_unit = DistanceUnitOptions.m
+    detector_offset = 0.0
+    detector_offset_unit = TimeUnitOptions.s
+    energy_unit = EnergyUnitOptions.eV
+
+    result = convert_array_from_time_to_energy(
+        time_array, time_unit, distance_source_detector, distance_source_detector_unit,
+        detector_offset, detector_offset_unit, energy_unit
+    )
+    assert np.isinf(result[0]) or np.isnan(result[0])
 
 
 if __name__ == "__main__":